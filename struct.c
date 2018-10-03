347877b97c519ce20fa361f8bf031eb32690d718
Jack Lu <jlu9@stuy.edu>
2018-10-03 15:32:17 -0400

    a

--- struct.c
+++ struct.c
@@ -5,49 +5,60 @@
 #include <stdlib.h>
 
 struct student {
-  char name[256];
-  float gpa;
+    char name[256];
+    float gpa;
 };
 
-char names[40][15] =
-{"Nadine", "Jack", "Michael", "Hasif",
-"Cooper", "Jason", "Raymond", "Joan",
-"Raunak", "Bill", "Kevin", "Mandy",
-"Tina", "Matthew", "Maggie", "Brian",
-"Puneet", "Calvin", "Jabir", "Camilla",
-"Hui Min", "Oliver", "Aaron", "Michael",
-"Horace", "Ahnaf", "Adil", "Imad",
-"Wei Wen", "Samuel", "Christopher", "Yulia",
-"Leah", "JonAlf", "David", "Peter",
-"Ivan", "Fabiha", "Rohan", "Michelle"};
+char names[40][15] = {
+    "Nadine", "Jack", "Michael", "Hasif",
+    "Cooper", "Jason", "Raymond", "Joan",
+    "Raunak", "Bill", "Kevin", "Mandy",
+    "Tina", "Matthew", "Maggie", "Brian",
+    "Puneet", "Calvin", "Jabir", "Camilla",
+    "Hui Min", "Oliver", "Aaron", "Michael",
+    "Horace", "Ahnaf", "Adil", "Imad",
+    "Wei Wen", "Samuel", "Christopher", "Yulia",
+    "Leah", "JonAlf", "David", "Peter",
+    "Ivan", "Fabiha", "Rohan", "Michelle"
+};
 
 struct student example()
 {
-  srand(time(NULL));
-  struct student bob;
-  strcpy(bob.name, names[rand() % 40]);
-  bob.gpa = rand() % 37 + 64;
-  return bob;
+    srand(time(NULL));
+    struct student bob;
+    strcpy(bob.name, names[rand() % 40]);
+    bob.gpa = rand() % 37 + 64;
+    return bob;
 }
 
 void modify(struct student *s, char *newName, float newGPA)
 {
-  strcpy(s->name, newName);
-  s->gpa = newGPA;
+    strcpy(s->name, newName);
+    s->gpa = newGPA;
 }
+
+void vowel(struct student *s){
+    int i = 0;
+    char *vowels = "aeiou";
+    char * mod = s->name;
+    while (mod++){
+        if (*mod){}
+    }
+}
+
 void printStu(struct student s)
 {
-  printf("%s's gpa: %f\n", s.name, s.gpa);
+    printf("%s's gpa: %f\n", s.name, s.gpa);
 }
 
 int main(){
-  printStu(example());
-  struct student test;
-  strcpy(test.name, "Voldemort");
-  test.gpa = 55;
-  char *mcName = "Harry";
-  printStu(test);
-  modify(&test, mcName, 99.5);
-  printStu(test);
-  return 0;
+    printStu(example());
+    struct student test;
+    strcpy(test.name, "Voldemort");
+    test.gpa = 55;
+    char *mcName = "Harry";
+    printStu(test);
+    modify(&test, mcName, 99.5);
+    printStu(test);
+    return 0;
 }
